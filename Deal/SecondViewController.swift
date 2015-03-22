8973ddf6dbb8684e42240adc8206b5fc1df33220
dokwon <dokwon@stanford.edu>
2015-03-22 00:48:43 -0700

    Initial Commit

---
+++ Deal/SecondViewController.swift
@@ -0,0 +1,25 @@
+//
+//  SecondViewController.swift
+//  Deal
+//
+//  Created by Do Kwon on 3/22/15.
+//  Copyright (c) 2015 Do Kwon. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
