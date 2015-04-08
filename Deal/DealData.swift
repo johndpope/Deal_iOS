8e2acb67e764762498ac7bca4d199339491fe387
dokwon <dokwon@stanford.edu>
2015-04-07 20:41:58 -0700

    first commit

---
+++ Deal/DealData.swift
@@ -0,0 +1,9 @@
+//
+//  DealData.swift
+//  Deal
+//
+//  Created by Do Kwon on 4/5/15.
+//  Copyright (c) 2015 Do Kwon. All rights reserved.
+//
+
+import Foundation
---
+++ Deal/DealInfo.swift
@@ -0,0 +1,9 @@
+//
+//  DealInfo.swift
+//  Deal
+//
+//  Created by Do Kwon on 4/5/15.
+//  Copyright (c) 2015 Do Kwon. All rights reserved.
+//
+
+import Foundation
